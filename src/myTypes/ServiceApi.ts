3adb59844c45dc99013bd08e3874226e6dac782d
ronnymarca <rfmartinezc@gmail.com>
2025-10-14 19:55:52 -0400

    updating images from website reyesms and updatin serviceapi and service.json

--- src/myTypes/ServiceApi.ts
+++ src/myTypes/ServiceApi.ts
@@ -1,54 +1,52 @@
-import type { ServiceData } from "../features/Services/types";
+import type { ServiceData } from '../features/Services/types';
 
 export const defaultServicesData: ServiceData[] = [
   {
     id: 1,
-    image: "/images/services/service1/office-5.jpg",
-    title: "Office, Commercial and Post-Construction Cleaning",
+    image: 'https://www.reyesms.ca/images/services/office/office-5.png',
+    title: 'Office, Commercial and Post-Construction Cleaning',
     description:
-      "Maintain a Pristine Environment. Our meticulous office, commercial and post-construction cleaning services in Ottawa keep workplaces spotless, healthy, and welcoming. From small offices to large facilities, we deliver tailored solutions with eco‑friendly products and reliable staff, ensuring your business always reflects professionalism and care.",
-    href: "/services/office"
+      'Maintain a Pristine Environment. Our meticulous office, commercial and post-construction cleaning services in Ottawa keep workplaces spotless, healthy, and welcoming. From small offices to large facilities, we deliver tailored solutions with eco‑friendly products and reliable staff, ensuring your business always reflects professionalism and care.',
+    href: '/services/office',
   },
   {
     id: 2,
-    image: "https://images.pexels.com/photos/6195288/pexels-photo-6195288.jpeg",
-    title: "Floor Stripping and Waxing",
+    image: 'https://www.reyesms.ca/images/services/floor/floor-2.png',
+    title: 'Floor Stripping and Waxing',
     description:
-      "Restore, Shine, and Protect your Floors. High‑traffic areas lose their luster quickly. Our Ottawa floor stripping and waxing services restore brilliance, extend floor life, and protect your investment. We specialize in vinyl, and laminate, ensuring polished, safe, and professional spaces for employees and visitors.",
-    href: "/services/floor"
+      'Restore, Shine, and Protect your Floors. High‑traffic areas lose their luster quickly. Our Ottawa floor stripping and waxing services restore brilliance, extend floor life, and protect your investment. We specialize in vinyl, and laminate, ensuring polished, safe, and professional spaces for employees and visitors.',
+    href: '/services/floor',
   },
   {
     id: 3,
-    image:
-      "https://images.pexels.com/photos/38325/vacuum-cleaner-carpet-cleaner-housework-housekeeping-38325.jpeg",
-    title: "Carpet Care",
+    image: 'https://www.reyesms.ca/images/services/carpet/carpet-1.png',
+    title: 'Carpet Care',
     description:
-      "Revitalize Carpets and Improve Indoor Air Quality. Our deep‑cleaning carpet services in Ottawa remove dirt, allergens, and odors for a healthier, fresher workplace. From small offices to large facilities, we restore vibrancy, extend carpet life, and protect your investment with trusted, professional care.",
-    href: "/services/carpet"
+      'Revitalize Carpets and Improve Indoor Air Quality. Our deep‑cleaning carpet services in Ottawa remove dirt, allergens, and odors for a healthier, fresher workplace. From small offices to large facilities, we restore vibrancy, extend carpet life, and protect your investment with trusted, professional care.',
+    href: '/services/carpet',
   },
   {
     id: 4,
-    image: "https://images.pexels.com/photos/4440529/pexels-photo-4440529.jpeg",
-    title: "Spring and Deep Cleaning",
+    image: 'https://www.reyesms.ca/images/services/deep-cleaning/deep-cleaning-1.png',
+    title: 'Spring and Deep Cleaning',
     description:
-      "Refresh your Workspace with Expert Spring/Deep Cleaning. Our Ottawa services go beyond routine upkeep, delivering a complete top‑to‑bottom refresh. Ideal for seasonal cleaning, pre‑event preparation, or post‑construction projects, we ensure offices, retail spaces, and facilities remain spotless, healthy, and welcoming.",
-    href: "/services/spring-deep"
+      'Refresh your Workspace with Expert Spring/Deep Cleaning. Our Ottawa services go beyond routine upkeep, delivering a complete top‑to‑bottom refresh. Ideal for seasonal cleaning, pre‑event preparation, or post‑construction projects, we ensure offices, retail spaces, and facilities remain spotless, healthy, and welcoming.',
+    href: '/services/spring-deep',
   },
   {
     id: 5,
-    image: "https://images.pexels.com/photos/2505120/pexels-photo-2505120.jpeg",
-    title: "Minor Renovations and Demolitions",
+    image: 'https://www.reyesms.ca/images/services/renovation/renovations-3.png',
+    title: 'Minor Renovations and Demolitions',
     description:
-      "Efficient Upgrades with Minimal Disruption. We provide safe, efficient solutions in Ottawa when your business needs light remodeling or non-structural removals. Our minor renovation and demolition services refresh offices, retail spaces, and facilities without the cost or downtime of full construction projects.",
-    href: "/services/renovation"
+      'Efficient Upgrades with Minimal Disruption. We provide safe, efficient solutions in Ottawa when your business needs light remodeling or non-structural removals. Our minor renovation and demolition services refresh offices, retail spaces, and facilities without the cost or downtime of full construction projects.',
+    href: '/services/renovation',
   },
   {
     id: 6,
-    image:
-      "https://images.pexels.com/photos/1556704/pexels-photo-1556704.jpeg",
-    title: "Light Maintenance and Bulb Replacement",
+    image: 'https://www.reyesms.ca/images/services/bulb/bulb-2.png',
+    title: 'Light Maintenance and Bulb Replacement',
     description:
-      "Small Fixes, Big Impact. Even the smallest details matter. Our Ottawa light maintenance and bulb replacement services keep workplaces bright, functional, and welcoming. From replacing flickering bulbs to handling minor repairs, we ensure your facility runs smoothly while maintaining a polished appearance.",
-    href: "/services/light-maintenance"
-  }
+      'Small Fixes, Big Impact. Even the smallest details matter. Our Ottawa light maintenance and bulb replacement services keep workplaces bright, functional, and welcoming. From replacing flickering bulbs to handling minor repairs, we ensure your facility runs smoothly while maintaining a polished appearance.',
+    href: '/services/light-maintenance',
+  },
 ];
